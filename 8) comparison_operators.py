1b95e2e9c4a4076b2ccc415637f5b7c608d07bf5
CollantesAI <miguel.a.collantes@outlook.com>
2024-03-06 13:53:38 -0500

    docs-shows comparison operators

---
+++ 8) comparison_operators.py
@@ -0,0 +1,12 @@
+x = 3 > 2
+print("x is " + str(x))
+a = 3 >= 2
+print("a is " + str(a))
+b = 3 < 2
+print("b is " + str(b))
+c = 3 <= 2
+print ("c is " + str(c))
+d = 3 == 2
+print("d is " + str(d))
+e = 3 != 2
+print("e is " + str(e))
