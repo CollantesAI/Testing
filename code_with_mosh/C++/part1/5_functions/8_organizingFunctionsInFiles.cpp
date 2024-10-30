ee675f1d63874da10ebf0436ac9eb1422d331758
CollantesAI <miguel.a.collantes@outlook.com>
2024-10-30 18:28:15 -0400

    chore-renamingFiles

---
+++ code_with_mosh/C++/part1/5_functions/8_organizingFunctionsInFiles.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "C:\Users\migue\OneDrive\Documents\GitHub\Testing\code_with_mosh\C++\part1\5_functions\8_utils\greet.hpp"
+
+using namespace std;
+
+int main() {
+    greet("Exit");
+
+    return 0;
+}
